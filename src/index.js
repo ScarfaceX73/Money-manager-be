4e830abf37c59db6280d5f27208e78b531b31bd3
Srinivasa Durai <kaizel73@gmail.com>
2023-03-12 10:53:38 +0530

    first

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ mongo.connect();
 
 app.use((req, res, next) => {
   console.log(req?.method);
-  let allowedOrigin = ["http://localhost:3000"];
+  let allowedOrigin = ["http://localhost:3000", "https://main--prismatic-sunflower-f951cc.netlify.app"];
   if (allowedOrigin.indexOf(req.headers.origin) != -1) {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.setHeader("Access-Control-Allow-Credentials", true);
